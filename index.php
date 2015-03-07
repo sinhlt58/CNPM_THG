21926de320de8b2a3687ab81e2ac30905a316780
trieudh_58 <trieudh_58@bitbucket.org>
2015-03-07 11:59:34 +0700

    Cap nhat project
    1. Sửa đổi giao diện Menu và thu gọn cấu trúc project
    2. Cập nhật giap diện Order
    3. Hoàn thành login form và sign up form

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include('template/header.php');// Page header?>		
-		
-<?php include('views/'.$view['name'].'.php'); // View type?>
-		
-<?php include('template/footer.php'); // Page footer?>	
--- views/page.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-			
-		<h1><?php echo $page['header']; ?></h1>
-		
-		<?php include('views/'.$page['slug'].'.php'); // View type?>
-			
-		<?//php echo $page['body_formatted']; ?>
-					 
-</div>
