900ede9cdcfbecb8ba4bf21c074a458531579c5f
sinhlt58 <sinhlt_58@vnu.edu.vn>
2015-03-13 23:08:45 +0700

    Update csdl

--- Mysql/thg.sql
+++ Mysql/thg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2015 at 12:47 PM
+-- Generation Time: Mar 13, 2015 at 04:07 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -28,11 +28,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `food` (
 `id` int(11) NOT NULL,
-  `user_id_food` int(11) NOT NULL,
   `fc_id` int(11) NOT NULL,
   `food_name` varchar(50) NOT NULL,
   `food_price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`id`, `fc_id`, `food_name`, `food_price`) VALUES
+(4, 2, 'Ra Muon', 200),
+(5, 2, 'Rau Cai', 100),
+(6, 2, 'Rau Sup No', 500),
+(7, 3, 'Tom Hum', 140),
+(8, 3, 'Cua Nuong', 345),
+(11, 6, 'Kem Tuoi', 456),
+(12, 6, 'Nuoc Cam', 999);
 
 -- --------------------------------------------------------
 
@@ -42,9 +54,45 @@ CREATE TABLE IF NOT EXISTS `food` (
 
 CREATE TABLE IF NOT EXISTS `food_categories` (
 `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `name` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` varchar(60) NOT NULL,
+  `restaurant_id` int(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food_categories`
+--
+
+INSERT INTO `food_categories` (`id`, `name`, `restaurant_id`) VALUES
+(2, 'Rau', 1),
+(3, 'Main', 1),
+(6, 'Trang Mieng 2', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_orders`
+--
+
+CREATE TABLE IF NOT EXISTS `food_orders` (
+`id` int(40) NOT NULL,
+  `order_id` int(40) NOT NULL,
+  `food_id` int(40) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=203 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food_orders`
+--
+
+INSERT INTO `food_orders` (`id`, `order_id`, `food_id`, `amount`) VALUES
+(195, 46, 7, 1),
+(196, 46, 8, 1),
+(197, 46, 11, 1),
+(198, 46, 12, 1),
+(199, 47, 7, 18),
+(200, 48, 8, 1),
+(201, 48, 12, 1),
+(202, 48, 11, 1);
 
 -- --------------------------------------------------------
 
@@ -74,6 +122,28 @@ INSERT INTO `navigation` (`id`, `label`, `url`, `target`, `position`, `status`) 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+`id` int(40) NOT NULL,
+  `restaurant_id` int(40) NOT NULL,
+  `table_order` int(40) NOT NULL,
+  `total_price` double NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `restaurant_id`, `table_order`, `total_price`) VALUES
+(46, 1, 46, 1940),
+(47, 1, 99, 2520),
+(48, 1, 46, 1800);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
@@ -86,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `title` varchar(200) NOT NULL,
   `header` varchar(300) NOT NULL,
   `body` mediumtext NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts`
@@ -94,11 +164,14 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 INSERT INTO `posts` (`id`, `user_id`, `type`, `slug`, `label`, `title`, `header`, `body`) VALUES
 (1, 1, 1, 'home', 'Home', 'Home', 'Welcome to THG 1.0', '<p>Long page.....</p>'),
-(2, 1, 1, 'menu', 'Menu', 'Menu', 'Phien ban tao thuc don v1.0', ''),
-(5, 1, 1, 'order', 'Order', 'Order', 'Coming soon ...', ''),
+(2, 1, 3, 'menu', 'Menu', 'Menu', 'Phien ban tao thuc don v1.0', ''),
+(5, 0, 3, 'order', 'Order', 'Order', 'V1.0', ''),
 (6, 0, 1, 'about-us', 'About Us', 'About Us', 'DNSTTT', ''),
 (7, 0, 1, 'sign-in', 'Sign In', 'Sign In', '', ''),
-(8, 9, 1, 'sign-up', 'Sign Up', 'Sign Up', '', '');
+(8, 9, 1, 'sign-up', 'Sign Up', 'Sign Up', '', ''),
+(9, 9, 3, 'new-order', 'New Order', 'New Order', '', ''),
+(10, 9, 2, 'restaurants', 'Restaurants', 'Restaurants', 'Choose one restaurant that you work for.', ''),
+(11, 9, 2, 'not-found', 'Not Found', 'Not Found', '404 we could not find this page.', '');
 
 -- --------------------------------------------------------
 
@@ -111,14 +184,35 @@ CREATE TABLE IF NOT EXISTS `post_types` (
   `label` varchar(150) NOT NULL,
   `name` varchar(150) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `post_types`
 --
 
 INSERT INTO `post_types` (`id`, `label`, `name`, `status`) VALUES
-(1, 'Pages', 'page', 1);
+(1, 'Pages', 'page', 1),
+(2, 'None navigation', 'none-navigation', 1),
+(3, 'After Choose Restaurant', 'page', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE IF NOT EXISTS `restaurants` (
+`id` int(40) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `number_of_table` int(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`id`, `name`, `number_of_table`) VALUES
+(1, 'RESTAURANT THG', 100);
 
 -- --------------------------------------------------------
 
@@ -163,6 +257,26 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `status`, `avatar`) VALUES
 (9, 'admin', 'admin', 'thg@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, '');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_restaurants`
+--
+
+CREATE TABLE IF NOT EXISTS `users_restaurants` (
+`id` int(40) NOT NULL,
+  `user_id` int(40) NOT NULL,
+  `restaurant_id` int(40) NOT NULL,
+  `role` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_restaurants`
+--
+
+INSERT INTO `users_restaurants` (`id`, `user_id`, `restaurant_id`, `role`) VALUES
+(1, 9, 1, 0);
+
 --
 -- Indexes for dumped tables
 --
@@ -180,9 +294,21 @@ ALTER TABLE `food_categories`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `food_orders`
+--
+ALTER TABLE `food_orders`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `navigation`
 --
 ALTER TABLE `navigation`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -198,6 +324,12 @@ ALTER TABLE `post_types`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `settings`
 --
 ALTER TABLE `settings`
@@ -210,6 +342,12 @@ ALTER TABLE `users`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users_restaurants`
+--
+ALTER TABLE `users_restaurants`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -217,32 +355,52 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `food_categories`
 --
 ALTER TABLE `food_categories`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `food_orders`
+--
+ALTER TABLE `food_orders`
+MODIFY `id` int(40) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=203;
 --
 -- AUTO_INCREMENT for table `navigation`
 --
 ALTER TABLE `navigation`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+MODIFY `id` int(40) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=49;
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `post_types`
 --
 ALTER TABLE `post_types`
-MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+MODIFY `id` int(40) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
 MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `users_restaurants`
+--
+ALTER TABLE `users_restaurants`
+MODIFY `id` int(40) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
